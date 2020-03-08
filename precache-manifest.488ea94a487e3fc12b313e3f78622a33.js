ee6688de876f2fcd8ae09c72de4483da15d233b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-08 21:03:56 +0000

    Deploy dhanushuUzumaki/Foodie to github.com/dhanushuUzumaki/Foodie.git:gh-pages

---
+++ precache-manifest.488ea94a487e3fc12b313e3f78622a33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856072d1c70f705ba9752e0e3c124cca",
+    "url": "https://dhanushuuzumaki.github.io/Foodie/index.html"
+  },
+  {
+    "revision": "6c0bb725210c87f3c576",
+    "url": "https://dhanushuuzumaki.github.io/Foodie/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bfea1a6f8bd6e755a360",
+    "url": "https://dhanushuuzumaki.github.io/Foodie/static/css/main.21af4b01.chunk.css"
+  },
+  {
+    "revision": "6c0bb725210c87f3c576",
+    "url": "https://dhanushuuzumaki.github.io/Foodie/static/js/2.087edffd.chunk.js"
+  },
+  {
+    "revision": "46493999a1765e17e70cd4dfebc97cd1",
+    "url": "https://dhanushuuzumaki.github.io/Foodie/static/js/2.087edffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfea1a6f8bd6e755a360",
+    "url": "https://dhanushuuzumaki.github.io/Foodie/static/js/main.12ed8e39.chunk.js"
+  },
+  {
+    "revision": "688eabfd65dbbd345fad",
+    "url": "https://dhanushuuzumaki.github.io/Foodie/static/js/runtime-main.1a329958.js"
+  }
+]);
